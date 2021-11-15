2eab86c9071b2220471bb3bf87fb28891a6aba38
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-11-16 09:58:05 +1000

    Доделал пример Thread_Join_002

--- CsharpProThreads/Thread_Join_002/Program.cs
+++ CsharpProThreads/Thread_Join_002/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 
+// Три пока с использованием Join
 namespace Thread_Join_002
 {
     class Program
@@ -24,27 +25,47 @@ namespace Thread_Join_002
 
         static void MethodThread3()
         {
+            Console.Write($"{Environment.NewLine}");
             Console.WriteLine($"Третий поток № {Thread.CurrentThread.GetHashCode()}");
+            
             WriteChar('3', 80, ConsoleColor.Yellow);
+            
+            Console.Write($"{Environment.NewLine}");
             Console.WriteLine("Третий поток завершился");
         }
         
         static void MethodThread2()
         {
-            Console.WriteLine($"Вторичный поток № {Thread.CurrentThread.GetHashCode()}");
+            Console.Write($"{Environment.NewLine}");
+            Console.WriteLine($"Второй поток № {Thread.CurrentThread.GetHashCode()}");
+            
             WriteChar('2', 80, ConsoleColor.Blue);
 
+            // Создаем третий поток
             var thread = new Thread(MethodThread3);
             thread.Start();
             thread.Join(); // Второй поток ждет пока завершится 3 поток
             
             WriteChar('2', 80, ConsoleColor.Blue);
+            
+            Console.Write($"{Environment.NewLine}");
             Console.WriteLine("Вторичный поток завершился");
         }
         
         static void Main(string[] args)
         {
+            Console.WriteLine($"Первый поток № {Thread.CurrentThread.GetHashCode()}");
             
+            WriteChar('1', 80, ConsoleColor.Green);
+
+            Thread thread = new Thread(MethodThread2);
+            thread.Start();
+            thread.Join();
+            
+            WriteChar('1', 80, ConsoleColor.Green);
+            
+            Console.Write($"{Environment.NewLine}");
+            Console.WriteLine("Первый поток завершился");
             
             Console.ReadLine();
         }
