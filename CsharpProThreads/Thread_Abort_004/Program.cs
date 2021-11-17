d084cc768141a07eb30da4eccaf9ae5644749700
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-17 22:18:17 +1000

    Добавил проект Thread_Abort_004

---
+++ CsharpProThreads/Thread_Abort_004/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Abort_004
+{
+    /// <summary>
+    /// Прерывание потока
+    /// </summary>
+    class Program
+    {
+        private static void Procedure()
+        {
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(Procedure);
+            thread.Start();
+            
+            Console.ReadLine();
+        }
+    }
+}
