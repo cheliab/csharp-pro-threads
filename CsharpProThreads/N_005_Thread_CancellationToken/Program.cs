3a88c65a1ec750e3fdb66ddec79a9c035a4955dd
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-11-19 10:25:05 +1000

    Добавил пример использования CancellationToken

---
+++ CsharpProThreads/N_005_Thread_CancellationToken/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace N_005_Thread_CancellationToken
+{
+    /// <summary>
+    /// Использование CancellationToken для прерывания потока
+    /// </summary>
+    class Program
+    {
+        private static void Procedure(object obj)
+        {
+            CancellationToken token = (CancellationToken)obj;
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            while (true)
+            {
+                if (token.IsCancellationRequested)
+                    break;
+                
+                Thread.Sleep(10);
+                Console.Write(".");
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Thread thread = new Thread(Procedure);
+            thread.Start(cts.Token);
+            
+            Thread.Sleep(2000);
+            
+            cts.Cancel();
+
+            thread.Join();
+            
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (true)
+            {
+                Thread.Sleep(20);
+                Console.Write("-");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
