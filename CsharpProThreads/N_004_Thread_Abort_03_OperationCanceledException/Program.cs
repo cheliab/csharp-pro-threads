f70e725b4465affb470de99e067f359da04d9112
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-19 23:46:44 +1000

    Добавил пример N_004_Thread_Abort_03_OperationCanceledException
    Бросаем исключение используя CancellationToken

---
+++ CsharpProThreads/N_004_Thread_Abort_03_OperationCanceledException/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace N_004_Thread_Abort_03_OperationCanceledException
+{
+    class Program
+    {
+        private static void Procedure(object obj)
+        {
+            CancellationToken token = (CancellationToken)obj;
+            
+            while (true)
+            {
+                try
+                {
+                    token.ThrowIfCancellationRequested();
+                    
+                    Thread.Sleep(10);
+                    Console.Write(".");
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    
+                    Console.WriteLine("\nOperationCanceledException");
+
+                    for (int i = 0; i < 160; i++)
+                    {
+                        Thread.Sleep(20);
+                        Console.Write(".");
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                // Отличие от Abort
+                // Тут продолжит выполняться цикл
+            }
+            Console.WriteLine("++++++ НЕ ВЫПОЛНИТСЯ +++++++");
+        }
+        
+        static void Main(string[] args)
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            
+            Thread thread = new Thread(Procedure);
+            thread.Start(tokenSource.Token);
+            
+            Thread.Sleep(2000);
+            
+            tokenSource.Cancel();
+
+            Console.ReadLine();
+        }
+    }
+}
