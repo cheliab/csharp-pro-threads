5de632d84c94940392770a9b11f870e4d377ed78
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-21 22:48:31 +1000

    Сделал набросок N_006_Thread_Priority
    Примеры изменения приоритетов у потоков

---
+++ CsharpProThreads/N_006_Thread_Priority/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace N_006_Thread_Priority
+{
+    class PriorityTest
+    {
+        public bool stop = false;
+
+        public void Method()
+        {
+            Console.WriteLine("Поток {0,3} с приоритетом {1,11} начал работу",
+                Thread.CurrentThread.ManagedThreadId,
+                Thread.CurrentThread.Priority);
+
+            long count = 0;
+
+            while (!stop)
+                count++;
+            
+            Console.WriteLine("Поток {0,3} с приоритетом {1,11} завершился. Count = {2,13}",
+                Thread.CurrentThread.ManagedThreadId,
+                Thread.CurrentThread.Priority,
+                    count.ToString("N0"));
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
