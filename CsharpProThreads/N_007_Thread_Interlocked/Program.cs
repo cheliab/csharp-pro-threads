b344a232e3aa65393c84d667032ff38de60129f8
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-23 08:10:53 +1000

    Накидал пример N_007_Thread_Interlocked

---
+++ CsharpProThreads/N_007_Thread_Interlocked/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+// Interlocked - Атомарные операции с переменными, доступными нескольким потокам
+namespace N_007_Thread_Interlocked
+{
+    class Program
+    {
+        /// <summary>
+        /// Счетчик запущенных потоков
+        /// </summary>
+        private static long counter;
+        
+        private static object block = new object();
+
+        private static void Procedure()
+        {
+            // Увеличение счетчика
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                // 1.
+                counter++;
+                
+                // 2.
+                // lock (block)
+                // {
+                //     counter++;
+                // }
+                
+                // Interlocked.Increment(ref counter);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ожидаемое значение счетчика = 10 000 000");
+
+            Thread[] threads = new Thread[10];
+            
+            for (int i = 0; i < 10; i++)
+                (threads[i] = new Thread(Procedure)).Start();
+
+            for (int i = 0; i < 10; i++)
+                threads[i].Join();
+            
+            Console.WriteLine($"Реальное значение счетчика = {counter}");
+            
+            Console.ReadKey();
+        }
+    }
+}
