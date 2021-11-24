db0365983baf3a4e241c907f2b5cbb45726fc08a
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-11-24 10:26:15 +1000

    Добавил набросок N_008_Thread_Interlocked_SpinLock

---
+++ CsharpProThreads/N_008_Thread_Interlocked_SpinLock/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+
+// Ручная реализация блокировки
+namespace N_008_Thread_Interlocked_SpinLock
+{
+    class SpinLock
+    {
+        /// <summary>
+        /// Флаг [0 - блок свободен, 1 - блок занят]
+        /// </summary>
+        private int block;
+
+        /// <summary>
+        /// Интервал проверки переменной "block"
+        /// </summary>
+        private int wait;
+
+        public SpinLock(int wait)
+        {
+            this.wait = wait;
+        }
+
+        /// <summary>
+        /// Установить блокировку (аналог - Monitor.Enter())
+        /// </summary>
+        public void Enter()
+        {
+            // Метод CompareExchage() [ Алгоритм работы ]
+            // 1. Сравнивает начальное значение первого аргумента с третьим аргументом.
+            // 2. Если первый аргумент (ref block) равен третьему аргументу (0), то в первый аргумент записывается значение второго аргумента (1) 
+            // 3. Иначе, если первый аргумент (ref block) не равен третьему аргументу (0), то первый аргумент остается прежним
+            // 4. Возвращает начальное значение первого аргумента (ref block) (всегда).
+            
+            int result = Interlocked.CompareExchange(ref block, 1, 0);
+
+            while (result == 1)
+            {
+                Thread.Sleep(wait);
+                result = Interlocked.CompareExchange(ref block, 1, 0);
+            }
+        }
+
+        /// <summary>
+        /// Сбросить блокировку (аналог - Monitor.Exit())
+        /// </summary>
+        public void Exit()
+        {
+            
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
