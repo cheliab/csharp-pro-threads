b560c2492b2c17722bd93b6c4d82bdf07567cccf
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-11-25 11:10:41 +1000

    Добавил проект N_009_Thread_Monitor

---
+++ CsharpProThreads/N_009_Thread_Monitor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace N_009_Thread_Monitor
+{
+    class Program
+    {
+        /// <summary>
+        /// Объект блокировки
+        /// </summary>
+        private static object block = new object();
+        
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
