45129f5178472269295ee4e10bb366600db165fe
Berezkin Pavel <cheliabkns@gmail.com>
2021-11-26 01:02:56 +1000

    Накидал 90% проекта N_010_Thread_Lock

---
+++ CsharpProThreads/N_010_Thread_Lock/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security;
+using System.Threading;
+
+namespace N_010_Thread_Lock
+{
+    class Program
+    {
+        private static object block = new object();
+        private static int counter;
+        private static Random random = new Random();
+
+        private static void Function()
+        {
+            lock (block)
+            {
+                counter++;
+            }
+
+            int time = random.Next(1000, 12000);
+            Thread.Sleep(time);
+
+            lock (block)
+            {
+                counter--;
+            }
+        }
+
+        private static void Report()
+        {
+            while (true)
+            {
+                int count;
+
+                lock (block)
+                {
+                    count = counter;
+                }
+                
+                Console.WriteLine("{0} потоков активно", count);
+                Thread.Sleep(100);
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var reporterThread = new Thread(Report)
+            {
+                IsBackground = true
+            };
+            reporterThread.Start();
+            
+            
+            
+        }
+    }
+}
